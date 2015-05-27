efb5a412da7e81d7944438e0db6869dbe3469864
Jonathan Kim <jckim134@gmail.com>
2015-05-27 14:29:17 -0700

    win screen / redzone / named functions

--- js/app.js
+++ js/app.js
@@ -1,11 +1,78 @@
 $(document).ready(function(){
 
-  alert("Hey Rupesh, I can't seem to get my relative feedback function to work. Could you look into that for me? Also, somehow my keydown function for pressing Enter is running even though I commented it out. The alerts were popping up twice before this.")
+// SET DEFAULT VALUES
+  var hint = {
+              correct: "You guessed right!",
+              closer: "Warmer",
+              farther: "Colder",
+              same: "Are you even trying?",
+              redZone: "REDZONE",
+              tryAgain: "Guess again!"
+              };
 
-	// Pick Random Number 1-100, Set default values
 	var chosen = Math.floor((Math.random() * 100) + 1);
 	var guessCount = 0;
 	$("#count").text(guessCount);
+  var lastGuess;
+
+  var removeHint = function(){
+    $('#feedback p').remove(0);
+  };
+
+  var giveHint = function(hint){
+    $('#feedback').append('<p>' + hint + '</p>');
+  };
+
+  var redZoneClass = function(){
+    $('body').css('background-color', '#000000');
+    $('html').css('background-color', '#FF0000');
+  };
+
+  var redZoneRemove = function(){
+    $('body').css('background-color', '#1F253D');
+    $('html').css('background-color', '#394264');
+  };
+
+  var winScreen = function(){
+    $('#feedback').css('background-color', '#00ff00');
+    $('html').css('background-color', '#00ff00');
+  };
+
+  var realGuesser = function(){
+    realGuess = parseInt($('#userGuess').val());
+    var guessRange = (Math.abs(realGuess - chosen));
+    var previousRange = (Math.abs(lastGuess - chosen));
+    if (realGuess >= 1 && realGuess <= 100) {
+      if(realGuess === chosen){
+          removeHint();
+          giveHint(hint.correct);
+          winScreen();
+      } else if(guessRange > 50 || realGuess === lastGuess) {
+          removeHint();
+          giveHint(hint.same);
+          redZoneRemove();
+      } else if(guessRange < previousRange && guessRange > 5){
+          removeHint();
+          giveHint(hint.closer);
+          redZoneRemove();
+      } else if(guessRange > previousRange && guessRange > 5){
+          removeHint();
+          giveHint(hint.farther);
+          redZoneRemove();
+      } else if(guessRange > 25 && guessRange < 50){
+          removeHint();
+          giveHint(hint.tryAgain);
+          redZoneRemove();
+      } else if(guessRange < 5 && realGuess != chosen){
+          removeHint();
+          giveHint(hint.redZone);
+          redZoneClass();
+      }
+    } else {
+      alert("You just wasted a guess! Enter a number between 1 - 100.");
+    }
+    lastGuess = realGuess;
+  };
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -20,7 +87,7 @@ $(document).ready(function(){
 
   	// New Game Function
   	var newGame = function(){
-  		chosen = Math.random(1, 101);
+  		chosen = Math.floor((Math.random() * 100) + 1);
   		guessCount = 0;
 		$("#count").text(guessCount);
     $('#feedback p').remove(0);
@@ -36,61 +103,11 @@ $(document).ready(function(){
       $('#guessList').append("<li>" + $('#userGuess').val() + "</li>");
     }
 
-  	// Guess Processor
-  	var processor = function() {
-  		var guess = parseInt($("#userGuess").val());
-  		if (guess >= 1 && guess <= 100) {
-  			if (guess === chosen) {
-          $('#feedback p').remove(0);
-  				$('#feedback').append("<p>You guessed right!</p>");
-        } else if (Math.abs(guess - chosen) >= 99) {
-          $('#feedback p').remove(0);
-          $('#feedback').append("<p>Are you even trying?</p>");
-  			} else if (Math.abs(guess - chosen) > 75) {
-          $('#feedback p').remove(0);
-          $('#feedback').append("<p>It's freezing in here!</p>");
-        } else if (Math.abs(guess - chosen) > 50) {
-          $('#feedback p').remove(0);
-  				$('#feedback').append("<p>You're as cold as ice!</p>");
-  			} else if (Math.abs(guess - chosen) > 25) {
-          $('#feedback p').remove(0);
-  				$('#feedback').append("<p>You're cold.</p>");
-  			} else if (Math.abs(guess - chosen) > 10) {
-          $('#feedback p').remove(0);
-  				$('#feedback').append("<p>You're warm..</p>");
-  			} else if (Math.abs(guess - chosen) > 5) {
-          $('#feedback p').remove(0);
-  				$('#feedback').append("<p>You're HOT!</p>");
-  			}
-  		} else {
-  			alert("You just wasted a guess! Please enter a number between 1 - 100");
-  		}
-  	};
-
-    // Relative Feedback
-    var relative = function() {
-      var guess2 = parseInt($("#userGuess").val());
-      var lastGuess;
-      if (guess2 >= 1 && guess2 <= 100) {
-        if (Math.abs(guess2 - chosen) < Math.abs(lastGuess - chosen)) {
-          $('#feedback2 p').remove(0);
-          $('#feedback2').append("<p>Closer than your last guess!</p>");
-        } else if (Math.abs(guess2 - chosen) > Math.abs(lastGuess - chosen)) {
-          $('#feedback2 p').remove(0);
-          $('#feedback2').append("<p>You're getting further away!</p>");
-        } else if (guess2 === lastGuess) {
-          $('#feedback2 p').remove(0);
-          $('#feedback2').append('<p>Really? That was your last guess.</p>');
-        }
-        var lastGuess = guess2;
-      }
-    };
 
     // Run processor on click
     $('#guessButton').on('click', function(e){
       e.preventDefault();
-      processor();
-      // relative();
+      realGuesser();
       guessCount++;
       $("#count").text(guessCount);
       addGuess();
